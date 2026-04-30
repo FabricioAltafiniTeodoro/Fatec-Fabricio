2abcb135040554662ce88093ca3d5e5d5a49e48d
fabricio171 <fabricioaltafiniteod@gmail.com>
2026-04-29 22:26:31 -0300

    teste

--- Aula Python/02-aleatorio/11-exer.py
+++ Aula Python/02-aleatorio/11-exer.py
@@ -3,9 +3,10 @@ n = [0]
 i = 1
 while i != 0:
     num = int(input("digite numero: "))
-    print("Lista Atualizada")
+  
 
     if num != 0:
+        print("Lista Atualizada")
         n.append(num)
         n.sort()
         for num in n:
